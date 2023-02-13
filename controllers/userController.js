1c7a45f3440a203d4dc9fe5d8c84b21b593d63fc
Ahmed Alasow <a.alasow01@gmail.com>
2023-02-13 13:52:05 +0000

    Updated user controller to handle the users created and deleted

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,8 @@ module.exports = {
   // Get a single user
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
+    .populate( {path: 'friends', populate: { path: 'friends' }} )
+    .populate( {path: 'thoughts'} )
       .select('-__v')
       .then((user) =>
         !user
@@ -32,7 +34,12 @@ module.exports = {
           ? res.status(404).json({ message: 'No user with that ID' })
           : Thought.deleteMany({ _id: { $in: user.thought } })
       )
-      .then(() => res.json({ message: 'User and associated apps deleted!' }))
-      .catch((err) => res.status(500).json(err));
+      .then((user) => 
+        !user
+          ?res
+            .status(404).json( {message: 'User created but no user was found with this ID'} )
+            : res.json({message: 'This user was deleted'})
+      )
+      .catch((err) => res.status(500).json(err))
   },
 };
